ebf7c1b22161123521703f9dc5c22b3e6be8953e
Esra Fernau <e.fernau@efero.de>
2017-01-23 21:29:35 +0100

    Update start-services.sh

--- scripts/start-services.sh
+++ scripts/start-services.sh
@@ -3,6 +3,5 @@
 [ -e /var/run/redis_6379.pid ] && rm /var/run/redis_6379.pid
 service redis_6379 start
 service mongod start
-mailcatcher --smtp-ip=0.0.0.0 --http-ip=0.0.0.0
 echo "waiting 5 seconds for the services can get ready..."
 sleep 5
